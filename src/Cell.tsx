f85a5c17d6a716a6a3dcde939b10d0e184843aa2
alajillo <alajillo2021@gmail.com>
2022-06-30 20:52:52 +0900

    update : bacground color

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -7,7 +7,7 @@ type CellPropsType = {
 }
 function Cell ({isActive = false, text, isSelect = false, isDefaultBackground = false, onSelect} : CellPropsType) {
     return (
-        <div className={`flex justify-center items-center w-full h-full ${isSelect && "animate-ping"} ${isActive && "bg-teal-100"} ${isDefaultBackground && !isActive ? 'bg-slate-100':'bg-white'}`} onClick={onSelect}>
+        <div className={`flex justify-center items-center w-full h-full ${isSelect && "animate-ping"} ${isActive && "bg-teal-200"} ${isDefaultBackground && !isActive ? 'bg-slate-100':'bg-white'}`} onClick={onSelect}>
             {text}
         </div>
     )
