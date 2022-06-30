83820b27deac7f2e2876da76e0641ee1d7513249
alajillo <alajillo2021@gmail.com>
2022-06-30 20:41:33 +0900

    add : sudoku grid

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -3,10 +3,11 @@ type CellPropsType = {
     isSelect? : boolean
     text : string
     onSelect? : () => void;
+    isDefaultBackground : boolean
 }
-function Cell ({isActive = false, text, isSelect = false, onSelect} : CellPropsType) {
+function Cell ({isActive = false, text, isSelect = false, isDefaultBackground = false, onSelect} : CellPropsType) {
     return (
-        <div className={`bg-white flex justify-center items-center border border-teal-200 w-full h-full ${isSelect && "animate-ping"} ${isActive && "bg-teal-100"}`} onClick={onSelect}>
+        <div className={`${isDefaultBackground ? 'bg-slate-200':'bg-white'} flex justify-center items-center border border-teal-200 w-full h-full ${isSelect && "animate-ping"} ${isActive && "bg-teal-100"}`} onClick={onSelect}>
             {text}
         </div>
     )
--- src/core/index.ts
+++ src/core/index.ts
@@ -2,21 +2,26 @@ export type CellType = {
     text : string,
     isActive : boolean,
     isSelect : boolean,
+    isDefaultBackground : boolean
 }
 
+function isMiddle(index : number){
+    return index > 2 && index < 6
+}
 
 export class Sudoku{
-    public static init() : CellType[][]{
-       return   new Array(9).fill(0).map(row=> new Array(9).fill(0).map(this.createCell))
+    public static init(){
+       return   new Array(9).fill(0).map((_,row)=> new Array(9).fill(0).map((_,column) => this.createCell(row,column)))
     }
-    private static createCell() : CellType{
+    private static createCell(row : number,column : number) : CellType{
         return {
             text : '0',
             isActive : false,
-            isSelect : false
+            isSelect : false,
+            isDefaultBackground : this.checkDefaultBackground(row,column)
         }
     }
-    public static selectCell(list : CellType[][], row : number,column : number) : CellType[][] {
+    public static selectCell(list : CellType[][], row : number,column : number){
             return list.map((rows,rowIndex) => rows.map((cell,columnIndex) => {
                 return {
                     ...cell,
@@ -25,7 +30,7 @@ export class Sudoku{
                 }
             }))
     }
-    public static inputCell(list : CellType[][], value : number) : CellType[][] {
+    public static inputCell(list : CellType[][], value : number){
         return list.map((rows) => rows.map((cell) => {
             return {
                 ...cell,
@@ -34,5 +39,8 @@ export class Sudoku{
                 text : cell.isSelect ? String(value) : cell.text
             }
         }))
+    }
+    private static checkDefaultBackground(row : number,column : number){
+        return isMiddle(row) ? isMiddle(column) : !isMiddle(column)
     }
 }
