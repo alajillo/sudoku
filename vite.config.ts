1dbbc0417f0452c2996a92d830bdb4208638ee29
alajillo <alajillo2021@gmail.com>
2022-07-01 23:49:18 +0900

    add : icons

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({registerType : 'autoUpdate', injectRegister: 'auto'})]
+  plugins: [react(), VitePWA({registerType : 'autoUpdate', injectRegister: 'auto',
+  manifest: {
+    name: '스도쿠',
+    short_name: '스도쿠',
+    description: 'My Awesome App description',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: './images/android-512-512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
+})]
 })
